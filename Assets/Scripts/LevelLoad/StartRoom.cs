1648ba3d29801fd08f3f0bc8a3bd1b81d030deb5
BerufsbildnerITSiemens <gian-andri.haenny@siemens.com>
2017-09-11 16:57:17 +0200

    made some changes

---
+++ Assets/Scripts/LevelLoad/StartRoom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartRoom : MonoBehaviour {
+
+    public VRSensor trigger;
+    public Transform target;
+    private bool entered = false;
+
+	// Use this for initialization
+	void Start () {
+        trigger.triggerEnter += triggerEnter;
+	}
+
+    private void triggerEnter(Collider collider, VRSensor sensor)
+    {
+        if (collider.gameObject.transform.name == "Camera (eye)" && entered == false)
+        {
+            transform.position = target.position;
+        }
+    }
+}
